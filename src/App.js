73606e6fe51e8fac8ff9ce601cef8a1822f02073
Rupinderkaushal <rupinder.xpertiks@gmail.com>
2022-05-26 18:08:01 -0700

    done

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import BridgeOne from './components/BridgeOne/BridgeOne';
 import BridgeTwo from './components/BridgeTwo/BridgeTwo';
 import LightMode from './components/CrogeNFTs/LightMode/LightMode';
 import HomePage from './components/HomePage/HomePage';
-
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-         {/* <BridgeOne/> */}
-         {/* <BridgeTwo/> */}
-         <HomePage/>
-         {/* <LightMode/>  */}
-         {/* <DarkMode/> */}
+          <BrowserRouter>
+         <Routes>
+           <Route exact to="/" element={<BridgeOne/>} />
+           <Route exact to="/bridgetwo" element={<BridgeTwo/>} />
+           <Route exact to="/homepage" element={<HomePage/>} />
+           <Route exact to="/lightmode" element={<LightMode/>} />
+           <Route exact to="/darkmode" element={<DarkMode/>} />
+         </Routes>
+         </BrowserRouter>
+
     </div>
   );
 }
