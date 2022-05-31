851d4ebc5efdf72f290435773a11bd24ea3659f9
Rupinderkaushal <rupinder.xpertiks@gmail.com>
2022-05-31 12:48:09 -0700

    darkmodebuttonfixed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,18 +2,29 @@ import React from 'react'
 import DarkMode from './CrogeNFTs/DarkMode/DarkMode';
 import LightMode from './CrogeNFTs/LightMode/LightMode';
 import { Switch } from 'antd';
+import {BsFillMoonStarsFill} from "react-icons/bs";
 import './CrogeNFTs/LightMode/LightNftComp/LightNftComp.css';
+import ToggleTheme from "react-toggle-theme";
+ 
 const Layout = () => {
     const [isDark,setIsDark] =React.useState(true);
-
+    const [currentTheme, setCurrentTheme] = React.useState("light");
     const onChange = () => {
         setIsDark(isDark=>!isDark)
+        if(isDark === true){
+          setCurrentTheme("light")
+        }
+        else{
+          setCurrentTheme("dark")
+        }
        
       };
   return (
-    <div className='layout-div'>
+    <div>
            <div className='toggler'>
-        <Switch  onChange={onChange} />
+           <ToggleTheme selectedTheme={currentTheme}  onChange={onChange} />
+        {/* <Switch style={{backgroundImage:<BsFillMoonStarsFill/>}}  onChange={onChange} /> */}
+
         </div>
         {/* <button onClick={onChange}>Toogle</button> */}
         {
---
+++ src/components/themeChanger/ThemeChanger.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ThemeChanger = () => {
+  return (
+    <div>ThemeChanger</div>
+  )
+}
+
+export default ThemeChanger
