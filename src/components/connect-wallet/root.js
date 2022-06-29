73d3ae59f5e6ecc67ede6505e23de06572c67ef0
pardeep889 <pardeep889@hotmail.com>
2022-06-29 16:17:57 +0530

    wallet

--- src/components/connect-wallet/root.js
+++ src/components/connect-wallet/root.js
@@ -38,8 +38,8 @@ export const providerOptions = {
   walletconnect: {
     package: WalletConnectProvider,
     options: {
-      // bridge: 'https://bridge.walletconnect.org',
-      // infuraId: '14a0951f47e646c1b241aa533e150219',
+      bridge: 'https://bridge.walletconnect.org',
+      infuraId: '14a0951f47e646c1b241aa533e150219',
       rpc:{
         25: "https://cronosrpc-1.xstaking.sg",
       }
