bec3d6f186724c129d8f7adf28a00ef9d9e42425
pardeep889 <pardeep889@hotmail.com>
2022-06-30 09:07:17 +0530

    update links

--- src/components/CrogeNFTs/NFTFooter/NftFooter.js
+++ src/components/CrogeNFTs/NFTFooter/NftFooter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './NftFooter.css';
-import {BsFacebook,BsInstagram,BsTwitter,BsLinkedin} from "react-icons/bs";
+import {BsFacebook,BsInstagram,BsTwitter,BsYoutube} from "react-icons/bs";
 import {FaTelegramPlane} from "react-icons/fa";
 const NftFooter = ({color}) => {
   return (
@@ -10,11 +10,11 @@ const NftFooter = ({color}) => {
       <p>Croge Website</p>
         </div>   
         <div className='footer__links'>
-          <a href='#'><BsFacebook size={24} /></a>
-          <a href='#'><BsInstagram size={24} /></a>
-          <a href='#'> <BsTwitter size={24} /></a>
-          <a href='#'> <BsLinkedin size={24} /></a>
-          <a href='#'> <FaTelegramPlane size={24}/></a>
+          <a target="_blank" href='https://www.facebook.com/croge.crypto/'><BsFacebook size={24} /></a>
+          <a target="_blank" href='https://www.instagram.com/croge.cro/'><BsInstagram size={24} /></a>
+          <a target="_blank" href='https://twitter.com/CrogeCoin'> <BsTwitter size={24} /></a>
+          <a target="_blank" href='https://www.youtube.com/channel/UCyuqUeF0rrC04e0VBpu8PcQ'><BsYoutube size={24} /></a>
+          <a target="_blank" href='https://t.me/crogecoin'> <FaTelegramPlane size={24}/></a>
           </div>     
       </div>
     </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Footer.css";
-import {BsFacebook,BsTwitter,BsLinkedin} from 'react-icons/bs'
+import {BsFacebook,BsYoutube,BsTwitter} from 'react-icons/bs'
 import {FaTelegramPlane} from 'react-icons/fa'
 import {AiOutlineInstagram } from 'react-icons/ai';
 import FooterIcon from "../../assests/tokenselect.png";
@@ -9,11 +9,11 @@ const Footer = () => {
     <div className='footer-wrapper'>
         <div className='footer-header'><p>Croge Website</p></div>
         <div className='footer-icons' >
-        <a href='#'>< BsFacebook  size={20} /></a>    
-            <a href='#'><AiOutlineInstagram size={20} /> </a>
-            <a href='#'> <BsTwitter size={20} /></a>
-             <a href='#'><BsLinkedin size={20} /></a>
-             <a href='#'><FaTelegramPlane size={20} /></a>
+        <a target="_blank" href='https://www.facebook.com/croge.crypto/'>< BsFacebook  size={20} /></a>    
+            <a target="_blank" href='https://www.instagram.com/croge.cro/'><AiOutlineInstagram size={20} /> </a>
+            <a target="_blank" href='https://twitter.com/CrogeCoin'> <BsTwitter size={20} /></a>
+             <a target="_blank" href='https://www.youtube.com/channel/UCyuqUeF0rrC04e0VBpu8PcQ'><BsYoutube size={20} /></a>
+             <a target="_blank" href='https://t.me/crogecoin'><FaTelegramPlane size={20} /></a>
         </div>
         <div className='footer-details'>
             <div>
